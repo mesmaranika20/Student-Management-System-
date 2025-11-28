307125b2cc26c722ef21f4ca844cfa26c530f585
Smaranika2006 <smaranika.s2006@gmail.com>
2025-11-28 10:16:09 +0000

    db connectivity

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+# Starts the API server and initializes the database
+
 from http.server import HTTPServer
 from router import StudentRouter
+from database.connection import init_database
 
-def run_server(port=8000):
-
-    server = HTTPServer(("", port), StudentRouter)
-    print(f" Server running at http://localhost:{port}")
+def run_server():
+    init_database()
+    server = HTTPServer(("", 8000), StudentRouter)
+    print(" Server running at http://localhost:8000")
     server.serve_forever()
 
-    if __name__ == "__main__":
-        run_server()
+if __name__ == "__main__":
+    run_server()
