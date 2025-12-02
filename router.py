c38f5ab2a867047611b3e3406ddfad491d0ba8f6
Smaranika2006 <smaranika.s2006@gmail.com>
2025-12-02 07:06:12 +0000

    Completed CRUD

--- router.py
+++ router.py
@@ -6,7 +6,10 @@ from urllib.parse import urlparse
 
 from controllers.students import (
     get_all_students
-    
+    , get_student
+    , create_student
+    , update_student
+    , delete_student
 )
 
 from core.static import serve_static
@@ -26,8 +29,30 @@ class StudentRouter(BaseHTTPRequestHandler):
 
         if path == "/api/students":
             return get_all_students(self)
+        
+        # # API: Get student by ID
+        if path.startswith("/api/students/"):
+            student_id = int(path.split("/")[-1])
+            return get_student(self, student_id)
 
        
+        return send_404(self)
+
+    def do_POST(self):
+        if self.path == "/api/students":
+            return create_student(self)
+        return send_404(self)
+    
+    def do_PUT(self):
+        if self.path.startswith("/api/students/"):
+            student_id = int(self.path.split("/")[-1])
+            return update_student(self, student_id)
+        return send_404(self)
+
+    def do_DELETE(self):
+        if self.path.startswith("/api/students/"):
+            student_id = int(self.path.split("/")[-1])
+            return delete_student(self, student_id)
         return send_404(self)
 
    
---
+++ test_commands.sh
@@ -0,0 +1,39 @@
+##################### API Observation Via Codespace URL
+##################### API Observation Via Hopscotch
+##################### API Observation Via CURL
+
+# A. Get All Students
+curl -X GET "https://laughing-space-funicular-q7jvp4vwp4gj3g4r-8000.app.github.dev/api/students"
+
+
+# B. Get One Student
+curl -X GET "http://localhost:8000/api/students/1"
+
+# C. Create Student
+curl -X POST "https://laughing-space-funicular-q7jvp4vwp4gj3g4r-8000.app.github.dev/api/students" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "Alice Johnson",
+    "email": "alice@example.com",
+    "course": "Computer Science",
+    "year": 2
+  }'
+
+# D. Update Student
+curl -X PUT "https://laughing-space-funicular-q7jvp4vwp4gj3g4r-8000.app.github.dev/api/students/1" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "Alice Updated",
+    "email": "alice_new@example.com",
+    "course": "Data Science",
+    "year": 3
+  }'
+
+# E. Delete Student
+curl -X DELETE "https://laughing-space-funicular-q7jvp4vwp4gj3g4r-8000.app.github.dev/api/students/1"
+
+
+##################### DB Observation Via SQLite Web
+- install https://github.com/coleifer/sqlite-web
+- pip install sqlite-web
+- sqlite_web students.db
